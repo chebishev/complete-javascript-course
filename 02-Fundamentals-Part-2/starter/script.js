7d0e6ca7db1db07b8e26e6683a42819db6539184
chebishev <atanas.chebishev@gmail.com>
2023-04-24 15:21:02 +0300

    arrays challenge

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -19,3 +19,29 @@ checkWinner(calcAverage(44, 23, 71), calcAverage(65, 54, 49));
 checkWinner(calcAverage(85, 54, 41), calcAverage(23, 34, 27));
 
 // arrays codding challenge
+
+// 50 - 300 = 15%
+// else = 20%
+
+function calcTip(bill) {
+    if (bill >= 50 && bill <= 300) {
+        return bill * 0.15;
+    }
+    else {
+        return bill * 0.2;
+    }
+}
+
+testData = [125, 555, 44];
+let bills = [];
+let tips = [];
+let total = []
+
+for(let i = 0; i < testData.length; i++) {
+    bills.push(testData[i]);
+    tips.push(calcTip(testData[i]));
+    total.push(testData[i] + tips[i]);
+}
+console.log(bills);
+console.log(tips);
+console.log(total);
