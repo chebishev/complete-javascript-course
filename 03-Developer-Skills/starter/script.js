8027e829ae0f4c2bece0d54d59934bdbf4541aab
chebishev <atanas.chebishev@gmail.com>
2023-04-28 12:53:05 +0300

    dev skill challenge

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,14 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
+ 
+// temperatures coding challenge:
 
+function printForecast(arr) {
+  let str = '';
+  for (let i = 0; i < arr.length; i++) {
+    str += `...${arr[i]}°C in ${i + 1} days...`;
+  }
+  //console.log(str);
+}
+printForecast([17, 21, 23]);
+printForecast([12, 5, -5, 0, 4]);
